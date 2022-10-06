513e151c729398f6a8534c34ffd3375848995ae8
TestingKeypup <106384399+TestingKeypup@users.noreply.github.com>
2022-10-06 08:30:47 +0200

    Update placeholder.cs
    fix on a faulty operation

--- placeholder.cs
+++ placeholder.cs
@@ -4,11 +4,16 @@
             var fake = new Fake<IFoo>();
 
             var callConfig = A.Fake<IAnyCallConfigurationWithNoReturnTypeSpecified>();
-            var config = A.Fake<IStartConfiguration<IFoo>>();
 
             A.CallTo(() => config.AnyCall()).Returns(callConfig);
             A.CallTo(() => this.startConfigurationFactory.CreateConfiguration<IFoo>(A<FakeManager>.That.Fakes(fake.FakedObject))).Returns(config);
 
+            // Act
+            var result = fake.AnyCall();
+            // Act
+            var result = fake.AnyCall();
+            // Act
+            var result = fake.AnyCall();
             // Act
             var result = fake.AnyCall();
 
